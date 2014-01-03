42506691b8eed0d3e247e337632366e99a9cf520
tom <tom@tom-VirtualBox.(none)>
2014-01-03 12:15:36 -0800

    finish editing paths, finish staging for sql execution

--- ftpImport.sh
+++ ftpImport.sh
@@ -6,39 +6,43 @@ LOG_PATH="./logs/"
 TMP_PATH="./tmp/"
 
 # first, retrieve list of available files from server. 
-ftp -pivd ftp.platform.mediamind.com > transfer.log << EOF
-nlist . oFile
+ftp -pivd ftp.platform.mediamind.com > $LOG_PATH/transfer.log << EOF
+nlist . $TMP_PATH/oFile
 quit
 EOF
 
+
 # strip out extra formatting
-cat oFile | grep MM_CLD_RICH.*131205.*\.zip > files.txt
-rm oFile
+cat $TMP_PATH/oFile | grep \.done > $TMP_PATH/files.txt
+#rm $TMP_PATH/oFile
 
-rm -f toDownload
-touch toDownload
 
-rm -f downloadCmds
-touch downloadCmds
+rm -f $TMP_PATH/toDownload
+touch $TMP_PATH/toDownload
+
+rm -f $TMP_PATH/downloadCmds
+touch $TMP_PATH/downloadCmds
+
+
 while read l; do
 	# if filename is not in .downloaded, add to list of files to retrieve
 	if [ -z `cat .downloaded | grep $l` ] 
 	then
-		echo "get $l ./downloaded/$l" >> downloadCmds
-		echo $l >> toDownload
+		echo "get $l $DATA_PATH/$l" >> $TMP_PATH/downloadCmds
+		echo $l >> $TMP_PATH/toDownload
 	fi
 	
-done < files.txt
+done < $TMP_PATH/files.txt
 
 # retrieve files. 
-ftp -vpi ftp.platform.mediamind.com < downloadCmds 2> ftpErrors.log
+ftp -vpi ftp.platform.mediamind.com < $TMP_PATH/downloadCmds 2> $LOG_PATH/ftpErrors.log
 
-if [[ `cat ftpErrors.log` != "" ]]
+if [[ `cat $TMP_PATH/ftpErrors.log` != "" ]]
 then
 	echo "problemos"
 	exit 
 fi
-
+exit
 rm downloadCmds
 cat toDownload >> .downloaded
 
