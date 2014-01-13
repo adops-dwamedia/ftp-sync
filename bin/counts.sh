dd230ffdc52bbc642b0a2f0119818a8413e85281
Tom Bedor <tomb@dwa-cookie.(none)>
2014-01-13 11:41:55 -0800

    get schema to dev box

--- bin/counts.sh
+++ bin/counts.sh
@@ -10,10 +10,10 @@ HOST="localhost"
 DB="DWA_SF_Cookie"
 PW=`cat ../../pw/mysql`
 
-mysql -u$USER -p$PW $DB -e "SELECT DATE(EventDate) d, COUNT(*) FROM MM_Standard_Copy GROUP BY d UNION ALL SELECT DATE(EventDate) d, COUNT(*) FROM MM_Standard GROUP BY d;" > $TMP_PATH/standard_count.txt
-
-mysql -u$USER -p$PW $DB -e "SELECT DATE(InteractionDate) d, COUNT(*) FROM MM_Rich_Copy GROUP BY d UNION ALL SELECT DATE(InteractionDate) d, COUNT(*) FROM MM_Rich GROUP BY d;" > $TMP_PATH/rich_count.txt
-
-mysql -u$USER -p$PW $DB -e "SELECT DATE(ConversionDate) d, COUNT(*) FROM MM_Conversion_Copy GROUP BY d UNION ALL SELECT DATE(ConversionDate) d, COUNT(*) FROM MM_Conversion GROUP BY d;" > $TMP_PATH/conversion_count.txt
-
+mysql -u$USER -p$PW $DB -e "SELECT DATE(EventDate) d, COUNT(*) FROM MM_Standard GROUP BY d;" > $TMP_PATH/standard_count.txt
+echo "Standard counts calculated"
+mysql -u$USER -p$PW $DB -e "SELECT DATE(InteractionDate) d, COUNT(*) FROM MM_Rich GROUP BY d;" > $TMP_PATH/rich_count.txt
+echo "Rich counts calculated"
+mysql -u$USER -p$PW $DB -e "SELECT DATE(ConversionDate) d, COUNT(*) FROM MM_Conversion GROUP BY d;" > $TMP_PATH/conversion_count.txt
+echo "Conversion counts calculated"
 echo "Counts complete"
