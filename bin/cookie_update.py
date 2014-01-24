85beefbd4123f80e3860d2371241c7d1dd288092
Tom Bedor <tombedor@Toms-MacBook-Air-2.local>
2014-01-24 11:16:48 -0800

    asdf

--- bin/cookie_update.py
+++ bin/cookie_update.py
@@ -7,8 +7,84 @@ import os
 import re
 import datetime
 from warnings import filterwarnings
-print "beginning update"
 filterwarnings('ignore', category = mdb.Warning)
+
+home_path="/usr/local/ftp_sync/bin"
+sql_path="/usr/local/ftp_sync/SQL/"
+data_path="/usr/local/var/ftp_sync/downloaded/"
+log_path="/usr/local/ftp_sync/logs/"
+tmp_path="/usr/local/ftp_sync/var/"
+
+user="tomb"
+host="10.0.101.15"
+db="DWA_SF_Cookie"
+pw=os.popen('/usr/local/pw/mysql').read()
+
+
+# connect to db
+try:
+	con = mdb.connect(host, user, pw, db)
+	cur = con.cursor()
+	con.set_character_set('utf8')
+	con.autocommit(True)
+	cur.execute('SET NAMES utf8;') 
+	cur.execute('SET CHARACTER SET utf8;')
+	cur.execute('SET character_set_connection=utf8;')
+
+        
+except mdb.Error, e:
+
+	print "Error %d: %s" % (e.args[0], e.args[1])
+	sys.exit(1)
+
+sys.ext()
+
+
+def get_ftp_commands():
+
+# checks import_log table and returns two arrays: one with ftp commands and one with the
+# list of files to be downloaded.
+	
+	print "retrieving list of files"
+	os.system("""ftp -pid ftp.platform.mediamind.com > $log_path/transfer.log 2>> $log_path/ftpErrors.log << EOF
+	nlist . $tmp_path/oFile
+	quit
+	EOF""")
+ 	return
+ 	
+ 
+get_ftp_commnds()
+'''
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+# def get_ftp_data()
+# def insert_csv()
+# def update_advertiser_tables()
+# def update_match()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 os.system("bash /usr/local/ftp_sync/bin/ftpImport.sh")
 
@@ -79,5 +155,4 @@ con.commit()
 
 if con:
 	con.close()
-
-
+'''
