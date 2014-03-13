1512de2b3d880f01e0f9c649b0d104225a38aa97
Tom Bedor <tombedor@gmail.com>
2014-03-13 15:52:53 -0700

    update insert to batch insert

--- bin/cookie_update.py
+++ bin/cookie_update.py
@@ -24,7 +24,7 @@ def ftp_sync(sync_dir):
 		if sf not in local_files and "zip" in sf:
 			cmd += "get %s\n"%sf
 	cmd = "'" + cmd + "'"
-	print cmd
+#	print cmd
 	p1 = subprocess.Popen(['echo', cmd], stdout=subprocess.PIPE)
 	p2 = subprocess.call(["ftp", "-p", "-i", "ftp.platform.mediamind.com"], stdin = p1.stdout)
 	return
@@ -39,18 +39,18 @@ def get_Advertiser_dict(cur):
 		adv_dict[a[1]] = tblName
 	return adv_dict
 def create_Ad_Tables(cur):
-	cur.execute("SELECT AdvertiserName, AdvertiserID FROM SF_Match.Advertisers WHERE Active = 1")
+	cur.execute("SELECT AdvertiserName, AdvertiserID FROM SF_Match.Advertisers")
 	advert = cur.fetchall()
 	for a in advert:
-		print a[0], a[1]
+#		print a[0], a[1]
 		tableName0="Std_" + a[0].replace(" ", "_")
 		tableName="DWA_SF_Cookie." +  tableName0
 		# check if table exists:
 		cur.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME LIKE '%s'"%tableName0);
 		res = cur.fetchall()
 		if len(res) == 0:
-			print "Creating table %s"%tableName
-			stmt1 = "CREATE TABLE IF NOT EXISTS %s"%tableName + " AS SELECT UserID, EventID, EventTypeID, STR_TO_DATE(EventDate, '%m/%d/%Y %h:%i:%s %p') AS EventDate, CampaignID, SiteID, PlacementID, IP, AdvertiserID FROM DWA_SF_Cookie.Std_Akamai WHERE 1=0"
+#			print "Creating table %s"%tableName
+			stmt1 = "CREATE TABLE IF NOT EXISTS %s"%tableName + " AS SELECT UserID, EventID, EventTypeID, STR_TO_DATE(EventDate, '%m/%d/%Y %h:%i:%s %p') AS EventDate, CampaignID, SiteID, PlacementID, IP, AdvertiserID,loadTime FROM DWA_SF_Cookie.Std_Akamai WHERE 1=0"
 			stmt2 = "ALTER TABLE %s ADD PRIMARY KEY(EventID)"%tableName
 			stmt3 = "ALTER TABLE %s ADD INDEX (UserID)"%tableName
 			stmt4 = "ALTER TABLE %s ADD INDEX (EventDate)"%tableName
@@ -72,6 +72,7 @@ def csv_Standard(file_name, cur, tbl_dict={},key_pos=3):
 	with open(file_name,'r') as f:
 		while True:
 			vals = f.readline()
+			stmt_dict = {}
 			if vals is None or vals == "": break
 			vals = [i.replace("\r\n","") for i in vals.split(",")]	
 			if not keys_set:
@@ -82,54 +83,43 @@ def csv_Standard(file_name, cur, tbl_dict={},key_pos=3):
 				for i in range(len(vals)):
 					row_d[col_names[i]] = "'%s'"%vals[i]
 				row_d['EventDate'] = "STR_TO_DATE(%s,'%%c/%%e/%%Y %%l:%%i:%%s %%p')"%row_d['EventDate']
-				stmt = "INSERT IGNORE INTO %s (loadTime, UserID, EventID, EventTypeID, EventDate, CampaignID, SiteID, PlacementID, IP, AdvertiserID) VALUES (CURRENT_TIMESTAMP(), "%tbl_dict[row_d['AdvertiserID'].replace("'","")]
-				stmt += "%s,"*9
+				if row_d['AdvertiserID'] not in stmt_dict.keys():
+					stmt = "INSERT IGNORE INTO %s (loadTime, UserID, EventID, EventTypeID, EventDate, CampaignID, SiteID, PlacementID, IP, AdvertiserID) VALUES "%tbl_dict[row_d['AdvertiserID'].replace("'","")]
+				else:
+					stmt = stmt_dict[row_d["AdvertiserID"]] + ","
+				stmt += "(NOW(), "+ "%s,"*9
 				stmt = stmt%(row_d['UserID'],row_d['EventID'], row_d['EventTypeID'], row_d['EventDate'], row_d['CampaignID'],row_d['SiteID'], row_d['PlacementID'], row_d['IP'], row_d['AdvertiserID'])
 				stmt = stmt[:-1]+")"
-				print stmt
+				stmt_dict[row_d['AdvertiserID']] = stmt
+		for k, stmt in stmt_dict.iteritems():
+			try:
 				cur.execute(stmt)	
+			except:
+				print "fail: %s"%stmt
+				return
 	return
+def remove_dups(tbl,dup_col, unique_col,cur):
+	# Dangerous. Removes duplicates of dup_col.
+	cur.execute("select %s FROM %s GROUP BY %s HAVING COUNT(*) > 1"%(dup_col,tbl,dup_col))
+	dup_vals = [d[0] for d in cur.fetchall()]
+	stmt = "CREATE TABLE tmp_dup_remove SELECT %s, %s FROM %s WHERE %s IN ("%(dup_col, unique_col, tbl, dup_col)
+	for d in dup_vals:
+		stmt += "'%s',"%d
+	stmt = stmt[:-1]+")"
+	cur.execute(stmt)
+	
 
-def insert_all():
-	print "insert all"
-	zip_dir = "/usr/local/var/ftp_sync/downloaded/"
-	ls_files = 	subprocess.check_output(['ls',zip_dir]).split()
-	print ls_files
-	for l in ls_files:
-		sql_errors = False
-		if "Standard" in l and "zip" in l:
-			print l
-			cmd = "unzip -xu %s -d%s/Standard"%(zip_dir + "/" + l, zip_dir)
-			print cmd			
-			os.system(cmd)
-			for csv_file in subprocess.check_output(['ls', zip_dir+"/Standard"]).split():
-				try:
-					insert_csv(zip_dir + "/Standard/" + csv_file, "MM_Standard_tmp", True)
-				except:
-					cmd = "echo 'import of %s failed' >> %s"%(csv_file,log_path) + "/mysqlImport.err"
-					os.system(cmd)
-					return
-					sql_errors = True
-			if not sql_errors:
-				stmt = "INSERT INTO import_log VALUES ('%s', CURRENT_DATE()) ON DUPLICATE KEY UPDATE importDate = CURRENT_DATE()"%l
-				print stmt
-				cur.execute(stmt)
-			mv_cmd = "mv "+zip_dir + l + " "+zip_dir+"/inSql/"
-			print mv_cmd
-			os.system(mv_cmd)
-		elif "Conversion" in l and "zip" in l:
-			cmd = "unzip -xu %s -d%s/Conversion"%(zip_dir + "/" + l, zip_dir)
-			print cmd
-			os.system(cmd)
-			for csv_file in subprocess.check_output(['ls', zip_dir+"/Conversion"]).split():
-				try:
-					insert_csv(zip_dir + "/Conversion/" + csv_file, "MM_Conversion")
-				except:
-					cmd = "echo 'import of %s failed' >> %s"%(csv_file,log_path) + "/mysqlImport.err"
-					os.system(cmd)
-					sql_errors = True
-	return
+	cur.execute("SELECT COUNT(*) FROM tmp_dup_remove")
+	before = cur.fetchall()[0]
 
+	stmt = "DELETE FROM tmp_dup_remove a WHERE (a.%s,a.%s) IN (SELECT min(b.%s), b.%s FROM tmp_dup_remove b GROUP BY b.%s)"%(unique_col,dup_col, unique_col,dup_col, dup_col)
+	print stmt
+	cur.execute(stmt)
+	cur.execute("SELECT COUNT(*) FROM tmp_dup_remove")	
+	after = cur.fetchall()[0]
+
+	print "found %s rows before, removed %s uniques, have %s left"%(before,len(dup_vals),after)
+	
 def update_all():
 	(file_ls,ftp_cmds) = get_ftp_commands()
 	get_ftp_data(ftp_cmds)
@@ -137,9 +127,14 @@ def main():
 	con,cur = mysql_login.mysql_login()
 	adv_dict = get_Advertiser_dict(cur)
 	con.autocommit(True)
-	create_Ad_Tables(cur)	
-	csv_Standard("/usr/local/var/ftp_sync/downloaded/test.csv", cur,adv_dict)
-#	ftp_sync("/usr/local/var/ftp_sync/downloaded")
+#	remove_dups("Std_Akamai", "eventID", "loadTime", cur)
+#	create_Ad_Tables(cur)	
+	files_dir = "/usr/local/var/ftp_sync/downloaded/Standard/"
+	files = subprocess.check_output(["ls", files_dir]).split()
+	print files
+	for f in files:
+		csv_Standard(files_dir + f, cur, adv_dict)
+	#ftp_sync("/usr/local/var/ftp_sync/downloaded")
 	if con:
 		con.commit()
 		con.close()		
