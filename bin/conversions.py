5039f1d0f1c34d9b26e54771d44553c08268d7b3
tombedor <tombedor@gmail.com>
2014-03-28 14:27:14 -0700

    fix conversions

--- bin/conversions.py
+++ bin/conversions.py
@@ -48,9 +48,9 @@ def initialize(cur,con):
 			"KEY eventDate (eventDate))" 
 			cur.execute(stmt)
 	#create table to be used later for excluding conversions from processing
-	cur.execute("CREATE TABLE IF NOT EXISTS conv_exclude_list(conversionID char(36) PRIMARY KEY)")
+	cur.execute("CREATE TABLE IF NOT EXISTS converting_users(userID char(36) PRIMARY KEY, lastUpdate DATETIME, INDEX (lastUpdate))")
 	con.commit()
-	print "done."
+	print "\tdone."
 def update(cur,con, conv_interval=30, insert_interval = 1000):
 	print "\tupdating data for tables..."
 	# create a dict. key is AdvertiserID, include:
@@ -59,26 +59,46 @@ def update(cur,con, conv_interval=30, insert_interval = 1000):
 	#	list lists: userID and timestamp of conversions.
 	# conv_interval is the max length of time an imps can influence a conversion.
 	ad_dict = {}
+	
+	# get list of users 
 	cur.execute("SELECT AdvertiserID, AdvertiserName FROM SF_Match.Advertisers")
 	for id, adName in cur.fetchall():
 		std_tblName = "Std_%s"%adName.replace(" ", "_")
 		conv_tblName = std_tblName.replace("Std_", "Std_Conv_")
 		
-		print "\t\tUpdating %s"%conv_tblName
+
 		ad_dict[id] = {
 			'std_tblName':std_tblName,
 			'conv_tblName':conv_tblName
 			}
 		
-		# get converting users
-		cur.execute(
-		"SELECT userID, max(conversionDate), conversionID FROM DWA_SF_Cookie.MM_Conversion " +\
+		# get new users and users that need to be updated. Two queries, one for returning
+		# converters and one for new converters.
+		
+		# new converters
+		stmt = "SELECT userID, max(conversionDate) FROM DWA_SF_Cookie.MM_Conversion " +\
 		"WHERE AdvertiserID = %s AND "%id +\
-		"conversionID NOT IN (SELECT a.conversionID FROM conv_exclude_list a) GROUP BY userID"
-		)
+		"userID NOT IN (SELECT userID FROM converting_users) GROUP BY userID"
+		#print stmt
+		cur.execute(stmt)
+		user_rows = list(cur.fetchall())
+#		if user_rows[0] == (None,None):
+#			user_rows.pop(0)
+		new_users = len(user_rows)
 		
-		user_rows = cur.fetchall()
-		
+		# returning converters
+		stmt = "SELECT a.userID, MAX(a.conversionDate) FROM DWA_SF_Cookie.MM_Conversion a JOIN "+\
+		"converting_users b ON a.userID = b.userID WHERE a.conversionDate > b.lastUpdate AND "+\
+		"AdvertiserID = %s GROUP BY (a.userID)"%id 
+		#print stmt
+		cur.execute(stmt)
+		returning_users = list(cur.fetchall())
+		if len(returning_users) > 0:
+			user_rows += list(cur.fetchall())
+
+		print "\t\tUpdating %s: "%conv_tblName +\
+		"%s new converters, %s returning converters"%(new_users, len(user_rows) - new_users)
+
 		
 		to_insert = []
 		i = 0
@@ -94,7 +114,7 @@ def update(cur,con, conv_interval=30, insert_interval = 1000):
 		for ur in user_rows:
 			i += 1
 			if i%print_interval ==0:
-				print "\t%s of %s records, %s%% completed."%(i,total, int(i*100.0/total))
+				print "\t\t\t%s of %s records, %s%% completed."%(i,total, int(i*100.0/total))
 			
 			stmt = "SELECT * FROM DWA_SF_Cookie.%s "%std_tblName +\
 			"WHERE userID = '%s' AND "%ur[0] +\
@@ -112,9 +132,9 @@ def update(cur,con, conv_interval=30, insert_interval = 1000):
 				cur.executemany(insert_Stmt, to_insert[:1000])
 				to_insert = to_insert[1000:]
 			con.commit()
-			stmt = "INSERT IGNORE INTO conv_exclude_list (" +\
-			"SELECT ConversionID FROM DWA_SF_Cookie.MM_Conversion WHERE" +\
-			" userID = '%s' AND conversionDate < '%s')"%(ur[0], ur[1])
+			stmt = "INSERT INTO converting_users VALUES ('%s','%s')"%(ur[0], ur[1]) +\
+			"ON DUPLICATE KEY update lastUpdate = '%s'"%ur[1]
+#			print stmt
 			cur.execute(stmt)				
 			con.commit()
 
@@ -143,7 +163,7 @@ def update(cur,con, conv_interval=30, insert_interval = 1000):
 
 
 def main():
-	print "Updating Std_FilterImps db..."
+	print "\nUpdating Std_FilterImps db..."
 	# create conversion tables if they do not exist.
 	# for each tbl, gather userID's of users who have converted or clicked
 	# insert ignore records of each userID into Std_Conversion table.
