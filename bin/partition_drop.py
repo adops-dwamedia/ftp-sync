4acbb51b01a544ea12bd617d8d17b024447a8c6b
Tom Bedor <tombedor@gmail.com>
2014-06-25 18:00:36 -0700

    add file to prune old partitions

--- bin/partition_drop.py
+++ bin/partition_drop.py
@@ -3,6 +3,7 @@
 
 import MySQLdb as mdb
 import mysql_login
+import time
 from warnings import filterwarnings
 filterwarnings('ignore', category = mdb.Warning)        
 
@@ -10,13 +11,33 @@ filterwarnings('ignore', category = mdb.Warning)
 
 
 
-def partitionDrop(d):
-	cur.execute("EXPLAIN PARTITIONS SELECT * FROM %s"%tblName)
-	parts = cur.fetchall()[0][3]
+def partitionDrop(d,db,cur):
+	cur.execute("USE %s"%db)
+
+	cur.execute("SHOW TABLES")
+
+	tbls = [t[0] for t in cur.fetchall()]
+
+	for t in tbls:
+		print t
+		cur.execute("EXPLAIN PARTITIONS SELECT * FROM %s"%t)
+		parts = cur.fetchall()[0][3]
+		if parts is not None:
+			parts_d = {p:time.strptime(p[1:],"%Y-%m-%d")  for p in parts.split(",") if p != "pMAX"}
+
+			parts_d = {k:parts_d[k] for k in parts_d if parts_d[k] < d }
+
+			for k in parts_d.keys():
+			return
 
 
 
 
 if __name__ == "__main__":
-        start = datetime.datetime.now()
         con,cur = mysql_login.mysql_login()
+	partitionDrop(time.strptime("2014-03-01", "%Y-%m-%d"),"DWA_SF_Cookie",cur)	
+
+	if (con):
+		con.commit()
+		con.close()
+
