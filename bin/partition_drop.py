fdd186f9d794b5f6775c67e61a027300ad8f03a3
Tom Bedor <tombedor@gmail.com>
2014-07-17 15:04:27 -0700

    partition drop

--- bin/partition_drop.py
+++ bin/partition_drop.py
@@ -16,7 +16,7 @@ def partitionDrop(d,db,cur):
 
 	cur.execute("SHOW TABLES")
 
-	tbls = [t[0] for t in cur.fetchall()]
+	tbls = [t[0] for t in cur.fetchall() if t[0] in [ "MM_Rich"]]
 
 	for t in tbls:
 		print t
@@ -26,16 +26,19 @@ def partitionDrop(d,db,cur):
 			parts_d = {p:time.strptime(p[1:],"%Y-%m-%d")  for p in parts.split(",") if p != "pMAX"}
 
 			parts_d = {k:parts_d[k] for k in parts_d if parts_d[k] < d }
+			print parts_d.keys()
 
 			for k in parts_d.keys():
-			return
+				stmt = "alter table %s drop partition `%s`"%(t, k)
+				print stmt
+				cur.execute(stmt)
 
 
 
 
 if __name__ == "__main__":
         con,cur = mysql_login.mysql_login()
-	partitionDrop(time.strptime("2014-03-01", "%Y-%m-%d"),"DWA_SF_Cookie",cur)	
+	partitionDrop(time.strptime("2014-07-01", "%Y-%m-%d"),"DWA_SF_Cookie",cur)	
 
 	if (con):
 		con.commit()
